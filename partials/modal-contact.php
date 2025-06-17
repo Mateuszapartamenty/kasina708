2ab52e317bf07761dc41d25ea912f4096c78f52c
Mateuszapartamenty <mateuszromanczyk75@gmail.com>
2025-06-17 13:35:36 +0200

    Add files via upload

---
+++ partials/modal-contact.php
@@ -0,0 +1,12 @@
+<!-- modal-contact.php -->
+<div class="modal" id="modal">
+  <img src="" alt="Powiększone zdjęcie" id="modalImage" />
+</div>
+
+<div class="contact-panel" id="contactPanel">
+  <button id="closeContact">✕</button>
+  <h2>Kontakt</h2>
+  <p><strong>Telefon:</strong> +48 504 378 344</p>
+  <p><strong>Email:</strong> apartamentykasina@gmail.com</p>
+  <p><strong>Facebook:</strong> <a href="https://www.facebook.com/apartamentykasina/" target="_blank">Apartamenty Kasina</a></p>
+</div>
